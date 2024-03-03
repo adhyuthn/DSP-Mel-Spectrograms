35d133f5f21c48e66848806a2bcbb107b64a557b
adhyuthn <adhyuthnarayan008@gmail.com>
2024-03-03 20:00:29 +0530

    final

--- exp_5/q5.m
+++ exp_5/q5.m
@@ -3,27 +3,29 @@ t = linspace(0, 1, 8000);
 t = t(1:1024);
 k = 1:1024;
 f = (k - 1) * fs/length(t);
-f_0 = f(1 : 512);
-signal = sin(2*pi*200*t) + sin(2*pi*500*t) + sin(2*pi*1000*t) + sin(2*pi*2000*t) + sin(2*pi*4000*t);
+f = f(1 : 512);
+signal = sin(2*pi*200*t) + sin(2*pi*500*t) + sin(2*pi*1000*t) + ...
+         sin(2*pi*2000*t) + sin(2*pi*4000*t);
 
-filter = h_blackman;
+FILTER = h_blackman;
 sig_spect = abs(myfft(signal));
-sig_spect = sig_spect(1:512);
+sig_spect = sig_spect(2:513); %% totally udayip
 
-windowed = sig_spect .* myfreqz(filter);
+windowed = sig_spect .* myfreqz(FILTER);
 
 figure;
 subplot(2,1,1);
-plot(f_0, sig_spect);
+plot(f, sig_spect, 'LineWidth', 2);
 xlabel('Frequency (Hz)');
-ylabel('Magnitude Spectrum');
-title('Frequency Spectrum of the Signal');
+ylabel('Magnitude');
+title('Frequency Spectrum of the Composite Signal');
 grid on;
 subplot(2,1,2);
-plot(f_0, abs(windowed));
+plot(f, abs(windowed), 'LineWidth',2);
 xlabel('Frequency (Hz)');
-ylabel('Magnitude Spectrum');
-title('Frequency Spectrum of the Windowed signal');
+ylabel('Magnitude');
+title('Frequency Spectrum of the output signal');
+sgtitle('Qns 4 - Blackmann window design')
 grid on;
 
 
